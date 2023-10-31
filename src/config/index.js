b7611e00c04c33899c82d5e236f3331e0f58f95c
legend524 <thecrazydev524@gmail.com>
2023-10-31 19:39:26 +0900

    fixed token symbol in config

--- src/config/index.js
+++ src/config/index.js
@@ -16,7 +16,7 @@ export const wethUsdcFarmPid = 1;
 export const YEAR = 60 * 60 * 24 * 365;
 export const YEAR_BN = new BigNumber(YEAR);
 
-export const mainTokenSymbol = "WILDxxxxx";
+export const mainTokenSymbol = "3WiLD";
 
 export const BASE_EXCHANGE_URL_BY_CHAIN = {
   56: "https://pancakeswap.finance/",
