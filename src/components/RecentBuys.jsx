32ea5c2fc0aba0f178332d9c72545e628043e378
nextidearly <talenteddev0125@gmai.com>
2023-11-16 14:42:13 -0800

    NFT page updated

---
+++ src/components/RecentBuys.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+const RecentBuys = (last) => {
+    return <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-6 p-4 place-items-center max-w-[900px] mx-auto">
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+        <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+            <img
+                src='/134.png'
+                alt="token"
+                className="hover:scale-105 transition ease-in-out cursor-pointer w-full rounded-lg border-opacity-30"
+            />
+        </div>
+    </div>
+}
+
+export default RecentBuys;
--- src/pages/NftClaim.jsx
+++ src/pages/NftClaim.jsx
@@ -7,11 +7,13 @@ import useRefresh from "hooks/useRefresh";
 import Loading from "components/Loading";
 import { didUserReject } from "utils/customHelpers";
 import { useEthersSigner } from "hooks/useEthers";
+import RecentBuys from "components/RecentBuys";
 export default function Zap() {
   const { address } = useAccount();
   const [nfts, setNfts] = useState(0);
   const [myTokenIds, setMyTokenIds] = useState([]);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [stats, setStats] = useState(true)
   const nftContract = useNFTContract();
   const { fastRefresh } = useRefresh();
   const signer = useEthersSigner();
@@ -53,55 +55,67 @@ export default function Zap() {
 
   return (
     <div className="container">
-      <h1 className="text-center text-3xl font-semibold">My NFTs</h1>
-      <div className="flex justify-center gap-4 mt-16">
-        {myTokenIds && myTokenIds.length > 0 ? (
-          myTokenIds.map((tokenId, index) => (
-            <div className="w-[200px] h-[200px]">
-              <NFTCard key={index} tokenId={tokenId} />
-            </div>
-          ))
-        ) : (
-          <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg border border-gray-600">
-            <img
-              src={"/logo.jpg"}
-              alt="token"
-              className="w-full border-[3px] border-white rounded-lg border-opacity-30"
-            />
+      <div className="flex my-6 py-1 bg-[#0d223de8] w-fit mx-auto rounded-full">
+        <button onClick={() => setStats(true)} className="text-center w-32 py-2 px-4 border-r rounded-l-full border-[#243753e8]  hover:bg-[#142235e8]  transition ease-in-out">My NFTs</button>
+        <button onClick={() => setStats(false)} className="text-center w-32 py-2 px-4 hover:bg-[#142235e8] transition ease-in-out rounded-r-full">Recent buys</button>
+      </div>
+
+      {
+        !stats ? <RecentBuys last={12} /> : <>
+
+          <div className="flex justify-center gap-4 mt-16">
+            {myTokenIds && myTokenIds.length > 0 ? (
+              myTokenIds.map((tokenId, index) => (
+                <div className="w-[200px] h-[200px]">
+                  <NFTCard key={index} tokenId={tokenId} />
+                </div>
+              ))
+            ) : (
+              <div className="w-full max-w-[300px] max-h-[400px] p-4 rounded-lg bg-[#0d223de8]">
+                <img
+                  src={"/logo.jpg"}
+                  alt="token"
+                  className="w-full rounded-full border-opacity-30"
+                />
+              </div>
+            )}
           </div>
-        )}
-      </div>
-      <div className="mt-12 md:mt-24">
-        {address && !nfts && (
-          <p className="mt-3 text-center text-2xl">
-            You don't have any NFTs to claim.
-          </p>
-        )}
 
-        {address && nfts ? (
-          <p className="mt-3 text-center text-2xl">
-            You have {nfts} NFT(s) to claim.
-          </p>
-        ) : (
-          ""
-        )}
-      </div>
+          <div className="mt-12 md:mt-24">
+            {address && !nfts && (
+              <p className="mt-3 text-center text-2xl">
+                You don't have any NFTs to claim.
+              </p>
+            )}
 
-      <div className="flex justify-center items-center pb-16 m-2 mt-12">
-        <button
-          className="main_btn rounded-xl w-full max-w-sm flex justify-center px-6 py-3 hover:scale-105 transition ease-in-out"
-          // disabled={!address && !nfts}
-          onClick={() => claimNFT()}
-        >
-          {isProcessing ? (
-            <Loading title="Claiming NFT..." />
-          ) : nfts ? (
-            "Claim NFT"
-          ) : (
-            "Nothing to claim"
-          )}
-        </button>
-      </div>
+            {address && nfts ? (
+              <p className="mt-3 text-center text-2xl">
+                You have {nfts} NFT(s) to claim.
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+
+          <div className="flex justify-center items-center pb-16 m-2 mt-12">
+            <button
+              className="main_btn rounded-xl w-full max-w-sm flex justify-center px-6 py-3 hover:scale-105 transition ease-in-out"
+              // disabled={!address && !nfts}
+              onClick={() => claimNFT()}
+            >
+              {isProcessing ? (
+                <Loading title="Claiming NFT..." />
+              ) : nfts ? (
+                "Claim NFT"
+              ) : (
+                "Nothing to claim"
+              )}
+            </button>
+          </div>
+        </>
+      }
+
+
     </div>
   );
 }
