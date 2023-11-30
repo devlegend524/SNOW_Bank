dde4b486696acbee7e6092fa15f302f741269f06
nextidearly <talenteddev0125@gmail.com>
2023-11-30 21:05:11 +0900

    change logo loading

--- src/components/LogoLoading.jsx
+++ src/components/LogoLoading.jsx
@@ -5,7 +5,7 @@ export default function LogoLoading({ title = "Loading..." }) {
     <div className="fixed z-[99999] top-0 left-0 w-full h-screen flex justify-center items-center bg-black/80">
       <div className="bg-secondary rounded-lg p-12">
         <img
-          src="/assets/loading.webp"
+          src="/assets/baseLoading.png"
           alt="Loading..."
           className="w-[150px] h-[150px] bounce_animation relative z-[9999] mx-auto"
         />
